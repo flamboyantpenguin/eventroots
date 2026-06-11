39d7b1a13fdad446b75843b8f373e976c320ca21
flamboyantpenguin <mail@flamboyantpenguin.in>
2026-06-11 22:13:28 +0530

    feat(editor): theme now changes according to banner

--- frontend/src/pages/event/Editor.jsx
+++ frontend/src/pages/event/Editor.jsx
@@ -1,10 +1,44 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useError } from "/src/hooks/misc/useErrorContext";
+import {
+  themeFromSourceColor2,
+  applyTheme,
+} from "/src/hooks/misc/useMaterialDynamic";
 import { useEventContext } from "/src/hooks/event/useEventContext";
 import { useViewport } from "/src/services/getViewPort";
 import Desktop from "/src/layout/editor/Desktop";
 import Mobile from "/src/layout/editor/Phone";
+import {
+  argbFromHex,
+  sourceColorFromImage,
+} from "@material/material-color-utilities";
+
+let themeMediaCleanup = null;
+
+async function loadThemeColor(img) {
+  const theme2 = themeFromSourceColor2(await sourceColorFromImage(img), [
+    {
+      name: "custom-1",
+      value: argbFromHex("#ff0000"),
+      blend: true,
+    },
+  ]);
+  const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+  applyTheme(theme2, { target: document.body, dark: mediaQuery.matches });
+
+  if (themeMediaCleanup) themeMediaCleanup();
+
+  const handleThemeChange = (e) => {
+    applyTheme(theme2, { target: document.body, dark: e.matches });
+  };
+
+  mediaQuery.addEventListener("change", handleThemeChange);
+
+  themeMediaCleanup = () =>
+    mediaQuery.removeEventListener("change", handleThemeChange);
+}
 
 const Editor = () => {
   const location = useLocation();
@@ -24,7 +58,19 @@ const Editor = () => {
         );
       });
     }
-  }, [eventId, loadEvent, formData.id]);
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+
+    img.onload = () => {
+      loadThemeColor(img);
+    };
+
+    img.onerror = (error) => {
+      console.error("Error loading image:", error);
+    };
+
+    img.src = formData.banner_url;
+  }, [eventId, loadEvent, formData.id, formData.banner_url]);
 
   if (contextError) {
     triggerError(
